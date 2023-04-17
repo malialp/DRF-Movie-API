67ca439c85f42f7a441471a756580e19518836e1
AliBaba05 <malibaba3444@gmail.com>
2023-04-17 17:47:01 +0300

    add Profile Model to Admin Panel

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Director, Movie, Review, MovieList
+from .models import Director, Movie, Review, MovieList, Profile
 
 admin.site.register(Director)
 admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(MovieList)
+admin.site.register(Profile)
