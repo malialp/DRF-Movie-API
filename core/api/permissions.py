1b07d7174819eabd0715310c5d64cf6d4a3b40c0
AliBaba05 <malibaba2234@gmail.com>
2023-04-17 22:51:33 +0300

    permission bug fix

--- core/api/permissions.py
+++ core/api/permissions.py
@@ -1,13 +1,16 @@
 from rest_framework import permissions
 
-class IsReviewAuthorOrReadOnly(permissions.IsAdminUser):
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.author.user or request.method in permissions.SAFE_METHODS or super().has_permission(request, view) and request.method in ('GET', 'DELETE')
 
-class IsOwnerOrReadOnly(permissions.IsAdminUser):
+class IsReviewAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner or request.method in permissions.SAFE_METHODS or super().has_permission(request, view) and request.method in ('GET', 'DELETE')
+        return request.user == obj.author.user or request.method in permissions.SAFE_METHODS or request.user.is_staff and request.method in ('GET', 'DELETE')
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner or request.method in permissions.SAFE_METHODS or request.user.is_staff and request.method in ('GET', 'DELETE')
     
-class IsUserOrReadOnly(permissions.IsAdminUser):
+
+class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.user == obj.user and request.method != 'DELETE') or request.method in permissions.SAFE_METHODS or super().has_permission(request, view) and request.method in ('GET', 'DELETE')
+        return request.user == obj.user or request.method in permissions.SAFE_METHODS
