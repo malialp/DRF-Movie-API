33fe096cb79cc9c4cdee81b171f0fefa6eb7a7f7
AliBaba05 <malibaba3444@gmail.com>
2023-04-17 17:48:40 +0300

    add media url to path

--- MovieAPI/urls.py
+++ MovieAPI/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
     path('auth', obtain_auth_token, name='auth')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
