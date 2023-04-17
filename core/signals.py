e9142c091d6fa783895400b009901f7a08f0f0f7
AliBaba05 <malibaba3444@gmail.com>
2023-04-17 17:54:32 +0300

    create profile reciever and edit user reciever

--- core/signals.py
+++ core/signals.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import User
-from core.models import WatchList
+from core.models import WatchList, Profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
 def create_watchlist(sender, instance, created, **kwargs):
     if created:
-        WatchList.objects.create(user=instance)
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=Profile)
+def create_watchlist(sender, instance, created, **kwargs):
+    if created:
+        WatchList.objects.create(profile=instance)
