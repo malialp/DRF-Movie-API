0223cf1f48b85da6c25d436df95b6855ea81c039
AliBaba05 <malibaba2234@gmail.com>
2023-04-24 18:56:13 +0300

    add Like model to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Director, Movie, Review, MovieList, Profile
+from .models import Director, Movie, Review, MovieList, Profile, Like
 
 admin.site.register(Director)
 admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(MovieList)
 admin.site.register(Profile)
+admin.site.register(Like)
