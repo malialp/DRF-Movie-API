40e0650a631c224f51f704ecb5ce2ba68501cb03
AliBaba05 <malibaba2234@gmail.com>
2023-04-24 19:09:16 +0300

    Delete unused code

--- core/api/fields.py
+++ core/api/fields.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
-from core.models import MovieList
 
 class LikeHyperlink(serializers.HyperlinkedRelatedField):
     view_name = 'movielist-detail'
@@ -11,9 +10,3 @@ class LikeHyperlink(serializers.HyperlinkedRelatedField):
          "pk": obj.movielist.id
         }
         return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
-    
-    # def get_object(self, view_name, view_args, view_kwargs):
-    #     lookup_kwargs = {
-    #         "pk":view_kwargs['pk']
-    #     }
-    #     return MovieList.objects.get(**lookup_kwargs)
