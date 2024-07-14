e7cd423f170c14c3b1a703ece6d20b5be7dd11a2
Muhammed Ali Alp <malibaba2234@gmail.com>
2024-07-14 17:16:28 +0300

    func name change

--- core/signals.py
+++ core/signals.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
-def create_watchlist(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
